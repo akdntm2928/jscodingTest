34657ce23cb1c46eaf6b6441294ba252309f66ed
akdntm2928 <swh9870@naver.com>
2022-11-29 15:35:19 +0900

    h

--- chat59.js
+++ chat59.js
@@ -33,3 +33,19 @@
 // 될 수 있는 한 변은 12, 13, 14, 15, 16, 17 로 6개입니다.
 // 따라서 7 + 6 = 13을 return합니다.
 // sqrt() 제곱근
+
+function solution(sides) {
+    let count = 0
+    const max = Math.max(...sides)
+    const min = Math.min(...sides)
+    // 만약 기존 요소 중 가장 긴 변이 있다면
+    for(let i = max-min+1 ; i <= max ; i ++) {
+        count++
+    }
+    // 나머지 한 변이 가장 긴 변인 경우
+    for(let i = max+1 ; i < max+min ; i ++) {
+        count++
+    }
+    return count
+}
+console.log(solution([7,11]));
