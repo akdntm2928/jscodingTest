1140be5a5dc850d0174041a1c66fb1b5e1d4cbcf
root <root@DESKTOP-7IMJ1U2.localdomain>
2022-11-29 14:47:21 +0900

    merge

---
+++ chat49.js
@@ -0,0 +1,63 @@
+// 영어가 싫어요
+// 문제 설명
+// 영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다. 문자열 numbers가 매개변수로 주어질 때, numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
+
+// 제한사항
+// numbers는 소문자로만 구성되어 있습니다.
+// numbers는 "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" 들이 공백 없이 조합되어 있습니다.
+// 1 ≤ numbers의 길이 ≤ 50
+// "zero"는 numbers의 맨 앞에 올 수 없습니다.
+// 입출력 예
+// numbers	result
+// "onetwothreefourfivesixseveneightnine"	123456789
+// "onefourzerosixseven"	14067
+
+// { "zero": 0 }, { "one": 1 }, { "two": 2 }, { "three": 3 }, { "four": 4 }, { "five": 5 }, { "six": 6 }, { "seven": 7 }, { "eight": 8 }, { "nine": 9 }
+// 
+
+let nums = {
+    "zero": "0", "one": "1", "two": "2", "three": "3", "four": '4', "five": "5", "six": "6", "seven": '7', "eight": '8', "nine": "9"
+};
+function solution(str) {
+    tmp="";
+    let answer ="";
+    for(let i = 0; i<str.length; i++){
+        tmp +=str[i];
+        if(nums[tmp]!=undefined){
+            answer +=nums[tmp];
+            tmp="";
+        }
+    }
+    return Number(answer);
+}
+
+console.log(solution("onefourzerosixseven"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
